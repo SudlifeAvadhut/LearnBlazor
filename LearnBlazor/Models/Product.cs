d329bded887397114b62bb85be1e4a6939984dd7
avadhutap459 <avadhutap459@gmail.com>
2025-07-07 09:59:21 +0530

    Add project files.

---
+++ LearnBlazor/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int productId { get; set; }
+        public string productName { get; set; }
+        public bool IsActive { get; set; }
+        public double price { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+    }
+}
