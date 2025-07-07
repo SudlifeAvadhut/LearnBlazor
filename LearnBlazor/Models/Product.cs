d2cfe575d7406757dfb5c82d5bad2b8448a409cf
SudlifeAvadhut <avadhut.parab@sudlife.in>
2025-07-07 18:38:26 +0530

    Updated Learn Blazor

--- LearnBlazor/Models/Product.cs
+++ LearnBlazor/Models/Product.cs
@@ -1,11 +1,30 @@
-﻿namespace LearnBlazor.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnBlazor.Models
 {
     public class Product
     {
+        public Product()
+        {
+            AvailableAfter = DateOnly.FromDateTime(DateTime.Now);
+        }
         public int productId { get; set; }
+        [Required]
         public string productName { get; set; }
+        
         public bool IsActive { get; set; }
+
+        [Range(1, 1000)]
         public double price { get; set; }
         public IEnumerable<Product_Prop> ProductProperties { get; set; }
+        public Category Category { get; set; }
+        public DateOnly AvailableAfter { get; set; }
+    }
+
+    public enum Category
+    {
+        Entree,
+        Appetizer,
+        Dessert
     }
 }
